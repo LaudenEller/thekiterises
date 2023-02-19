be179dbb003a9d235114ba8a6aab61b7d2bb7968
Lauden Eller <laudlauden@gmail.com>
2023-02-18 20:24:26 -0800

    Added temp welcome animation code that will be replaced by with iframe code

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import './index.css';
 import './fonts/lulo-clean-w01-one-bold.ttf'
 
 
+// Q: what is happening with .createRoot and with .StrictMode?
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
